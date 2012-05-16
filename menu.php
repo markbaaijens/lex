8fdfb6e918f101429ee8d1cd584846fdec46f0fa
Mark Baaijens <mark.baaijens@gmail.com>
2012-05-16 21:41:48 +0200

    Origineel van Evert Jan Klein Velderman

---
+++ menu.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Embedded</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+algemene toegang<br>
+<br>
+<a href="index.php" target="mainFrame">home</a><br>
+<a href="news.php" target="mainFrame">prikbord</a><br>
+<a href="listings.php?type=Want" target="mainFrame">vraag</a><br>
+<a href="listings.php?type=Offer" target="mainFrame">aanbod</a><br>
+<a href="contact.php" target="mainFrame">contact</a><br>
+<br>
+enkel toegankelijk voor leden (na inloggen)<br>
+<br>
+<a href="member_login.php" target="mainFrame">login</a><br>
+<a href="member_logout.php" target="mainFrame">logout</a><br>
+<a href="listings_menu.php" target="mainFrame">advertenties</a><br>
+<a href="exchange_menu.php" target="mainFrame">transacties</a><br>
+<a href="member_directory.php" target="mainFrame">ledenlijst</a><br>
+<a href="member_profile.php" target="mainFrame">profiel</a><br>
+<a href="admin_menu.php" target="mainFrame">administratie</a><br>
+
+</body>
+</html>
