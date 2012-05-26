62065ad3c9d4d8908d870ed848aaf23348ffe1e7
Mark Baaijens <mark.baaijens@gmail.com>
2012-05-26 22:36:41 +0200

    Refactoring layout 'Member profile'

--- member_profile.php
+++ member_profile.php
@@ -10,15 +10,20 @@ $list .= "<A HREF=member_edit.php?mode=self><FONT SIZE=2>".$lng_edit_my_pers_inf
 
 if (ALLOW_IMAGES==true)
 	$list .= "<A HREF=member_photo_upload.php?mode=self><FONT SIZE=2>".$lng_upload_change_photo."</FONT></A><BR>";
+$list .= "<a href=member_summary.php?member_id=".$cUser->member_id."><font size=2>".$lng_view_my_profile_as_others."</font></a><br>";
+$list .= "<br>";
+$list .= "<b>".$lng_joint."</b><br>";
 
-$list .= "<A HREF=member_contact_create.php?mode=self><FONT SIZE=2>".$lng_add_joint_member_to_my_acc."</FONT></A><BR>";
-$list .= "<A HREF=member_contact_choose.php><FONT SIZE=2>".$lng_edit_delete_joint_member."</FONT></A><P>";
+$list .= "<A HREF=member_contact_create.php?mode=self><FONT SIZE=2>".$lng_add_joint_member_to_my_acc."</FONT></A><br>";
+$list .= "<A HREF=member_contact_choose.php><FONT SIZE=2>".$lng_edit_delete_joint_member."</FONT></A><br>";
+$list .= "<br>";
 
+$list .= "<b>".$lng_create_an_income_share."</b><br>";
 if (ALLOW_INCOME_SHARES==true)
 	$list .= "<A HREF=income_ties.php><FONT SIZE=2>".$lng_manage_income_shares."</FONT></A><p>";
-
-/*[chris]*/
-$list .= "<a href=member_summary.php?member_id=".$cUser->member_id."><font size=2>".$lng_view_my_profile_as_others."</font></a><p>";
+else	
+  $list .= $lng_income_share_inactive."<br>";
+	
 
 $p->DisplayPage($list);
 
