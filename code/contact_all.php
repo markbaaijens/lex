78f5bba350f7577c09c54f1b0681e7ef7775e98e
Mark Baaijens <mark.baaijens@gmail.com>
2013-04-23 22:06:12 +0200

    Removed dynamic email-from address, this serves no purpose and can be counter productive (email can be marked as spam)

--- code/contact_all.php
+++ code/contact_all.php
@@ -12,9 +12,6 @@ include("includes/inc.forms.php");
 //
 $form->addElement("header", null, $lng_this_email_goes_to_all_members." ".SITE_LONG_TITLE.".", null);
 $form->addElement("static", null, null, null);
-// This element (email_from) cannot simply converted to a static one: if the data (form variable EMAIL_FROM) contains
-// double quotes ", the text/data is malformed and the mail will NOT be send.
-$form->addElement("text", "email_from", $lng_email_from_address, array("size" => 50, "maxlength" => 50)); 
 $form->addElement("text", "subject", $lng_subject, array("size" => 50, "maxlength" => 50));
 $form->addElement("static", null, null, null);
 $form->addElement("textarea", "message", $lng_your_message, array("cols"=>65, "rows"=>10, "wrap"=>"soft"));
@@ -35,7 +32,6 @@ if ($form->validate()) {
  	$form->process("process_data", false);
 } else {  
   // Set default Values
-  $current_values["email_from"] = EMAIL_FROM;
 	$form->setDefaults($current_values);  
 	
 	// Display the form
@@ -58,12 +54,10 @@ function process_data ($values) {
 			$errors .= ", ";
 		
 		if($member->person[0]->email != "") {
-		  $email_from = $values["email_from"];
-			if ($email_from == "") 
-			  // Fill email_from from a sensible default, otherwise there is a chance this will not be send or 
-			  // otherwise will be marked as spam by the provider
-			  $email_from = EMAIL_FROM;
-  		$mailed = mail($member->person[0]->email, $values["subject"], wordwrap($values["message"], 64) , "From:". $email_from); 
+  		$mailed = mail($member->person[0]->email, 
+  		                $values["subject"], 
+  		                wordwrap($values["message"], 64) , 
+  		                "From:". EMAIL_FROM); 
 		} 
 		else
 			$mailed = true;
